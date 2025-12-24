8477cd81821593853ea7c4addf88d42c35205c8c
danyakhairullin <khdaniilnice@gmail.com>
2025-12-24 11:08:56 +0300

    Подправил 7 лабораторную работу

--- ex_7_2.py
+++ ex_7_2.py
@@ -12,4 +12,40 @@ class Manager(Employee):
         super().__init__(name, id_number)
         self.deportment = deportment
     def manage_project(self):
-        return f'Сотрудник в должности менеджера, отвечающий за '
+        return f'Сотрудник в должности менеджера, отвечающий за: {self.deportment}. '
+
+
+class Technician(Employee):
+    def __init__(self, name, id_number, specialization):
+        super().__init__(name, id_number)
+        self.specialization = specialization
+    def perform_maintenance(self):
+        return f'Выполняет техническое обслуживание: {self.specialization}. '
+
+
+class TechManager(Manager, Technician):
+    def __init__(self, name, id_number,deportment, specialization):
+        Employee.__init__(self, name, id_number)
+        self.deportment = deportment
+        self.specialization = specialization
+        self.list = []
+    def add_employee(self, man):
+        self.list.append(man)
+    def get_tean_info(self):
+        return f'Список всех сотрудников: {self.list}. '
+
+
+emp1 = Employee('Сергей', 1)
+emp2 = Manager('Валентин', 2, 'Котиков')
+emp3 = Technician('Вова', 3, 'Микровалновки')
+emp4 = TechManager('Никита', 4, 'Собачки', 'ЧайникиСобачки')
+
+print(emp1.get_info())
+print(emp2.manage_project())
+print(emp3.perform_maintenance())
+
+emp4.add_employee(emp1)
+emp4.add_employee(emp2)
+emp4.add_employee(emp3)
+
+print(emp4.get_tean_info())
